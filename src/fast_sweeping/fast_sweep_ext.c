e576af7f1f07454d8336c41c68fe6a838b65f10e
Eigenblutwurst <Hannes.Bathke@gmx.net>
2017-02-09 17:06:02 +0300

    WIP include fast_sweep extension into setup, debugging

--- src/fast_sweeping/fast_sweep_ext.c
+++ src/fast_sweeping/fast_sweep_ext.c
@@ -57,6 +57,61 @@ void Vect_from_Mat(npy_intp VectPos[1], npy_intp sel_row, npy_intp sel_col, npy_
     return;
 }
 
+void eq_solve(float64_t NewVal[1], float64_t x, float64_t a, float64_t b, float64_t f, float64_t h){
+    if (fabs(a-b) >= f*h){
+        NewVal[0]  = (a < b) ? a : b;
+        NewVal[0] += f*h;
+    }
+    else{
+        NewVal[0]  = a +b +pow( (2.0*f*f*h*h - pow((a-b),2.0)  ),0.5);
+        NewVal[0] /= 2.0;
+    }
+    return;
+}
+
+void upwind(float64_t NewVal[1], float64_t *StartTime, npy_intp i, npy_intp j, float64_t *Slowness, float64_t PatchSize, npy_intp NumInStk, npy_intp NumInDip){
+    npy_intp     i1, i2, j1, j2;
+    npy_intp     VectPos0[1];
+    npy_intp     VectPos1[1], VectPos2[1];
+    npy_intp     VectPos3[1], VectPos4[1];
+    float64_t    u_xmin, u_ymin;
+
+    i1 = i - 1;
+    i2 = i + 1;
+    j1 = j - 1;
+    j2 = j + 1;
+
+    if (i1 < 0){
+        i1 = 0;
+    }
+
+    if (i2 >= NumInStk){
+        i2 = NumInStk-1;
+    }
+
+    if (j1 < 0){
+        j1 = 0;
+    }
+
+    if (j2 >= NumInDip){
+        j2 = NumInDip-1;
+    }
+
+    Vect_from_Mat(VectPos0, i,  j, NumInDip);
+    Vect_from_Mat(VectPos1, i1, j, NumInDip);
+    Vect_from_Mat(VectPos2, i2, j, NumInDip);
+    Vect_from_Mat(VectPos3, i, j1, NumInDip);
+    Vect_from_Mat(VectPos4, i, j2, NumInDip);
+
+    u_xmin = (StartTime[VectPos1[0]] < StartTime[VectPos2[0]]) ?  StartTime[VectPos1[0]] : StartTime[VectPos2[0]];
+    u_ymin = (StartTime[VectPos3[0]] < StartTime[VectPos4[0]]) ?  StartTime[VectPos3[0]] : StartTime[VectPos4[0]];
+
+    eq_solve(NewVal, StartTime[VectPos0[0]], u_xmin, u_ymin, Slowness[VectPos0[0]],PatchSize);
+
+    NewVal[0] = (NewVal[0] <  StartTime[VectPos0[0]]) ? NewVal[0] :  StartTime[VectPos0[0]];
+    return;
+}
+
 void fast_sweep(float64_t *Slowness, float64_t *StartTime, float64_t PatchSize, npy_intp HypoInStk, npy_intp HypoInDip, npy_intp NumInStk, npy_intp NumInDip){
     int num_iter;
     npy_intp i, j, ii;
@@ -73,7 +128,7 @@ void fast_sweep(float64_t *Slowness, float64_t *StartTime, float64_t PatchSize, 
     num_iter = 0;
     PatchNum = NumInStk*NumInDip;
 
-    Time_old = (float64_t *) malloc((size_t) ((PatchNum)*sizeof(float32_t)));
+    Time_old = (float64_t *) malloc((size_t) ((PatchNum)*sizeof(float64_t)));
 
     int cnt = 0;
     for (i = 0; i < NumInStk; i++){
@@ -143,70 +198,15 @@ void fast_sweep(float64_t *Slowness, float64_t *StartTime, float64_t PatchSize, 
     return;
 }
 
-void upwind(float64_t NewVal[1], float64_t *StartTime, npy_intp i, npy_intp j, float64_t *Slowness, float64_t PatchSize, npy_intp NumInStk, npy_intp NumInDip){
-    npy_intp     i1, i2, j1, j2;
-    npy_intp     VectPos0[1];
-    npy_intp     VectPos1[1], VectPos2[1];
-    npy_intp     VectPos3[1], VectPos4[1];
-    float64_t    u_xmin, u_ymin;
-
-    i1 = i - 1;
-    i2 = i + 1;
-    j1 = j - 1;
-    j2 = j + 1;
-
-    if (i1 < 0){
-        i1 = 0;
-    }
-
-    if (i2 >= NumInStk){
-        i2 = NumInStk-1;
-    }
-
-    if (j1 < 0){
-        j1 = 0;
-    }
-
-    if (j2 >= NumInDip){
-        j2 = NumInDip-1;
-    }
-
-    Vect_from_Mat(VectPos0, i,  j, NumInDip);
-    Vect_from_Mat(VectPos1, i1, j, NumInDip);
-    Vect_from_Mat(VectPos2, i2, j, NumInDip);
-    Vect_from_Mat(VectPos3, i, j1, NumInDip);
-    Vect_from_Mat(VectPos4, i, j2, NumInDip);
-
-    u_xmin = (StartTime[VectPos1[0]] < StartTime[VectPos2[0]]) ?  StartTime[VectPos1[0]] : StartTime[VectPos2[0]];
-    u_ymin = (StartTime[VectPos3[0]] < StartTime[VectPos4[0]]) ?  StartTime[VectPos3[0]] : StartTime[VectPos4[0]];
-
-    eq_solve(NewVal, StartTime[VectPos0[0]], u_xmin, u_ymin, Slowness[VectPos0[0]],PatchSize);
-
-    NewVal[0] = (NewVal[0] <  StartTime[VectPos0[0]]) ? NewVal[0] :  StartTime[VectPos0[0]];
-    return;
-}
-
-void eq_solve(float32_t NewVal[1], float64_t x, float64_t a, float64_t b, float64_t f, float64_t h){
-    if (fabs(a-b) >= f*h){
-        NewVal[0]  = (a < b) ? a : b;
-        NewVal[0] += f*h;
-    }
-    else{
-        NewVal[0]  = a +b +pow( (2.0*f*f*h*h - pow((a-b),2.0)  ),0.5);
-        NewVal[0] /= 2.0;
-    }
-    return;
-}
-
 PyObject* w_fast_sweep(PyObject *dummy, PyObject *args){
     PyObject *slowness_arr;
     PyArrayObject *c_slowness_arr, *tzero_arr;
 
-    float_t64 patch_size, *slowness, *tzero;
-    npy_intp hyp_in_strike, hyp_in_dip, num_in_strike, num_in_dip, arr_size[1];
+    float64_t patch_size, *slowness, *tzero;
+    npy_intp h_strk, h_dip, num_strk, num_dip, arr_size[1];
 
-    if (!PyArg_ParseTuple(args, 'OdIIII', &slowness_arr, &patch_size, &h_strk, &h_dip, &num_strk, &num_dip)){
-        PyErrString(FastSweepExtError, 'Invalid call to fast_sweep! \n usage: fast_sweep(slowness_arr, patch_size, h_strk, h_dip, num_strk, num_dip)');
+    if (!PyArg_ParseTuple(args, "OdIIII", &slowness_arr, &patch_size, &h_strk, &h_dip, &num_strk, &num_dip)){
+        PyErr_SetString(FastSweepExtError, "Invalid call to fast_sweep! \n usage: fast_sweep(slowness_arr, patch_size, h_strk, h_dip, num_strk, num_dip)");
         return NULL;
     }
 
@@ -232,30 +232,12 @@ PyObject* w_fast_sweep(PyObject *dummy, PyObject *args){
 
 static PyMethodDef FastSweepExtMethods[] = {
     {"fast_sweep", w_fast_sweep, METH_VARARGS,
-"Fast Sweeping Algorithm to calculate rupture onset-times on patches of a plane given slowness of the rupturing patches.\n\n
-Parameters \n
----------- \n
-slowness : :py:class:`numpy.NdArray` \n
-    1-d vector with the inverse of rupture velocity \n
-patch_size : float \n
-    size of the square patches [m] \n
-h_strk : int \n
-    index of patch of the hypocenter along strike direction. Starts at 0! \n
-h_dip : int \n
-    index of patch of the hypocenter along dip direction. Starts at 0! \n
-num_strk : int \n
-    number of patches along strike direction. Starts at 0! \n
-num_dip : int \n
-    number of patches along dip direction. Starts at 0! \n\n
-Results\n
--------\n
-tzero : :py:class:`numpy.NdArray` \n"},
+"Fast Sweeping Algorithm to calculate rupture onset-times on patches of a plane given slowness of the rupturing patches.\n"},
 
-    {NULL, NULL, NULL}  /* Sentinel */
+    {NULL, NULL, 0, NULL}  /* Sentinel */
 };
 
-PyMODINIT_FUNC
-init_fast_sweep_ext(void){
+PyMODINIT_FUNC init_fast_sweep_ext(void){
     PyObject* m;
 
     m = Py_InitModule("fast_sweep_ext", FastSweepExtMethods);
