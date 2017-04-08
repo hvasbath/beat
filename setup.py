8940184ec9ff54c117f04821f63cb4f44f6d0825
Eigenblutwurst <Hannes.Bathke@gmx.net>
2017-04-08 23:47:29 +0300

    added bash completion

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 import os
 from distutils.core import setup, Extension
+from distutils.command.build_py import build_py
+import shutil
 
 try:
     import numpy
@@ -15,9 +17,26 @@ except ImportError:
             return ''
 
 
+class custom_build_py(build_py):
+    def run(self):
+        build_py.run(self)
+        try:
+            shutil.copy('extras/beat', '/etc/bash_completion.d/beat')
+            print 'Installing beat bash_completion...'
+        except IOError as e:
+            import errno
+            if e.errno in (errno.EACCES, errno.ENOENT):
+                print e
+            else:
+                raise e
+
+
 subpackages = ['beat.fast_sweeping']
 
 setup(
+    cmdclass={
+        'build_py': custom_build_py
+                },
     name='beat',
     description='Bayesian Earthquake Analysis Tool',
     version='0.1',
