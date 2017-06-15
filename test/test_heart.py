63f297d7c21e918aad02d98325e3ed30f8472502
hvasbath <Hannes.Bathke@gmx.net>
2017-06-15 14:44:27 +0300

    test running

--- test/test_heart.py
+++ test/test_heart.py
@@ -30,49 +30,71 @@ def get_run_directory():
         return cwd
 
 
-class TestHeart(unittest.TestCase):
+def load_problem(dirname, mode):
+    beat_dir = get_run_directory()
+    project_dir = os.path.join(beat_dir, 'data/examples', dirname)
+    return models.load_model(project_dir, mode=mode)
+
+
+def _get_mt_source_params():
+    source_point = {
+        'magnitude': 4.8,
+        'mnn': 0.84551376,
+        'mee': -0.75868967,
+        'mdd': -0.08682409,
+        'mne': 0.51322155,
+        'mnd': 0.14554675,
+        'med': -0.25767963,
+        'east_shift': 10.,
+        'north_shift': 20.,
+        'depth': 8.00,
+        'time': -2.5,
+        'duration': 5.,
+        }
+    return {k:num.atleast_1d(num.asarray(v)) for k, v in source_point.iteritems()}
+
+
+class TestSeisComposite(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         unittest.TestCase.__init__(self, *args, **kwargs)
-        self.beat_dir = get_run_directory()
-        self.examples_dir = os.path.join(self.beat_dir, 'data/examples')
+        self.dirname = 'FullMT'
+        self.mode = 'geometry'
 
-    def get_project_dir(self, name):
-        return os.path.join(self.examples_dir, name)
-
-    def _get_mt_source_params(self):
-        source_point = {
-            'magnitude': 4.8,
-            'mnn': 0.84551376,
-            'mee': -0.75868967,
-            'mdd': -0.08682409,
-            'mne': 0.51322155,
-            'mnd': 0.14554675,
-            'med': -0.25767963,
-            'east_shift': 10.,
-            'north_shift': 20.,
-            'depth': 8.00,
-            'time': -2.5,
-            'duration': 5.,
-            }
-        return {k:num.atleast_1d(num.asarray(v)) for k, v in source_point.iteritems()}
-
-    def test_full_mt(self):
+    @classmethod
+    def setUpClass(cls):
+        dirname = 'FullMT'
         mode = 'geometry'
+        cls.problem = load_problem(dirname, mode)
+        cls.sc = cls.problem.composites['seismic']
 
-        project_dir = self.get_project_dir('FullMT')
-
-        problem = models.load_model(project_dir, mode=mode)
-
-        sc = problem.composites['seismic']
-
-        point = self._get_mt_source_params()
-
-        synths, obs = sc.get_synthetics(problem.model.test_point, outmode='data')
+    def test_synths(self):
+        logger.info('Test synth')
+        synths, obs = self.sc.get_synthetics(
+            self.problem.model.test_point, outmode='data')
 
         for st, ot in zip(synths, obs):
             assert_allclose(st.ydata, ot.ydata, rtol=1e-05, atol=0)
 
+    def test_results(self):
+        logger.info('Test results')
+        results = self.sc.assemble_results(self.problem.model.test_point)
+
+        for result in results:
+            assert_allclose(result.processed_obs.ydata,
+                            result.processed_syn.ydata, rtol=1e-05, atol=0)
+            assert_allclose(result.filtered_obs.ydata,
+                            result.filtered_syn.ydata, rtol=1e-05, atol=0)
+
+    def test_weights(self):
+        logger.info('Test weights')
+        for wmap in self.sc.wavemaps:
+            for w, d in zip(wmap.weights, wmap.datasets):
+                assert_allclose(
+                    w.get_value(), d.covariance.chol_inverse,
+                    rtol=1e-08, atol=0)
+
+
 if __name__ == "__main__":
-    util.setup_logging('test_heart', 'info')
+    util.setup_logging('test_heart', 'warning')
     unittest.main()
