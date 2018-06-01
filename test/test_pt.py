3c2ca3e9144c7fcb01ef5fff59fbe26be5c36c58
hvasbath <Hannes.Bathke@gmx.net>
2018-06-01 12:48:10 -0600

    pt: adapted test from SMC

---
+++ test/test_pt.py
@@ -0,0 +1,104 @@
+import pymc3 as pm
+import numpy as num
+import os
+from beat.sampler import pt, metropolis, utility, backend
+from tempfile import mkdtemp
+import shutil
+import logging
+import theano.tensor as tt
+import unittest
+from pyrocko import util
+
+
+logger = logging.getLogger('test_pt')
+
+
+class TestPT(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
+        self.test_folder_multi = mkdtemp(prefix='PT_TEST')
+
+        logger.info('Test result in: \n %s' % self.test_folder_multi)
+
+        self.n_cpu = 3
+        self.n_samples = 1e3
+        self.tune_interval = 25
+        self.beta_tune_interval = 5e1
+        self.swap_interval = (100, 300)
+        self.buffer_size = self.n_samples / 20.
+
+    def _test_sample(self, n_jobs, test_folder):
+        logger.info('Running on %i cores...' % n_jobs)
+
+        n = 4
+
+        mu1 = num.ones(n) * (1. / 2)
+        mu2 = -mu1
+
+        stdev = 0.1
+        sigma = num.power(stdev, 2) * num.eye(n)
+        isigma = num.linalg.inv(sigma)
+        dsigma = num.linalg.det(sigma)
+
+        w1 = stdev
+        w2 = (1 - stdev)
+
+        def two_gaussians(x):
+            log_like1 = - 0.5 * n * tt.log(2 * num.pi) \
+                        - 0.5 * tt.log(dsigma) \
+                        - 0.5 * (x - mu1).T.dot(isigma).dot(x - mu1)
+            log_like2 = - 0.5 * n * tt.log(2 * num.pi) \
+                        - 0.5 * tt.log(dsigma) \
+                        - 0.5 * (x - mu2).T.dot(isigma).dot(x - mu2)
+            return tt.log(w1 * tt.exp(log_like1) + w2 * tt.exp(log_like2))
+
+        with pm.Model() as PT_test:
+            X = pm.Uniform('X',
+                           shape=n,
+                           lower=-2. * num.ones_like(mu1),
+                           upper=2. * num.ones_like(mu1),
+                           testval=-1. * num.ones_like(mu1),
+                           transform=None)
+            like = pm.Deterministic('like', two_gaussians(X))
+            llk = pm.Potential('like', like)
+
+        with PT_test:
+            step = metropolis.Metropolis(
+                n_chains=n_jobs - 1,
+                likelihood_name=PT_test.deterministics[0].name,
+                proposal_name='Normal',
+                tune_interval=self.tune_interval)
+
+        pt.pt_sample(
+            step,
+            n_jobs=n_jobs,
+            n_samples=self.n_samples,
+            swap_interval=self.swap_interval,
+            beta_tune_interval=self.beta_tune_interval,
+            homepath=test_folder,
+            progressbar=True,
+            buffer_size=self.buffer_size,
+            model=PT_test,
+            rm_flag=False)
+
+        stage_handler = backend.TextStage(test_folder)
+
+        mtrace = stage_handler.load_multitrace(-1, model=PT_test)
+
+        d = mtrace.get_values('X', combine=True, squeeze=True)
+        mu1d = num.abs(d).mean(axis=0)
+
+        num.testing.assert_allclose(mu1, mu1d, rtol=0., atol=0.03)
+
+    def test_multicore(self):
+        self._test_sample(self.n_cpu, self.test_folder_multi)
+
+    #def tearDown(self):
+     #   shutil.rmtree(self.test_folder_multi)
+
+
+if __name__ == '__main__':
+    util.setup_logging('test_pt', 'debug')
+    unittest.main()
