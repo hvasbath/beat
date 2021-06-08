3df9d33db7dccff7a310525add3011c809715014
hvasbath <Hannes.Bathke@gmx.net>
2021-06-08 15:18:02 +0200

    models.problem: FFI implemented lsq init for seismic data

--- beat/models/base.py
+++ beat/models/base.py
@@ -189,13 +189,9 @@ def sample(step, problem):
             logger.info('Random starting point.\n')
             start = None
         elif pc.mode_config.initialization == 'lsq':
+            logger.info('Least-squares-solution \n')
             from tqdm import tqdm
 
-            logger.info('Least-squares-solution \n')
-            if 'seismic' in pc.datatypes:
-                logger.warning(
-                    'Least-squares initialization is not'
-                    ' supported (yet) for seismic data, only!')
             start = []
             for i in tqdm(range(step.n_chains)):
                 point = problem.get_random_point()
