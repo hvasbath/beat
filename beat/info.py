0c18840af0c637c35591e546abd4927c84abc86c
hvasbath <Hannes.Bathke@gmx.net>
2021-09-03 12:39:28 +0200

    remove info.py, adjust .gitignore

--- beat/info.py
+++
@@ -1,7 +0,0 @@
-# This module is automatically created from setup.py
-project_root = '/home/mahdi/programs/beat'
-git_sha1 = '6c137738df38facc3cae76982a202004740aeea4'
-local_modifications = False
-version = '1.1'
-long_version = 'beat-1.1-6c137738df38facc3cae76982a202004740aeea4-2021-08-02_04:08:48'  # noqa
-installed_date = '2021-08-02_04:08:48'
