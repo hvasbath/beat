8f18162a2fde196c58cefae7f977bf5f79bfe532
hvasbath <Hannes.Bathke@gmx.net>
2023-01-21 21:02:20 +0100

    plotting.seismic.fuzzy_mt_decomp fix figure size

--- beat/plotting/seismic.py
+++ beat/plotting/seismic.py
@@ -1912,7 +1912,7 @@ def draw_fuzzy_mt_decomposition(problem, po):
 
     if not os.path.exists(outpath) or po.force or po.outformat == "display":
 
-        height = n_sources * 0.75
+        height = 1.5 + (n_sources - 1) * 0.65
         fig = plt.figure(figsize=(6.0, height))
         fig.subplots_adjust(left=0.01, right=0.99, bottom=0.03, top=0.97)
         axes = fig.add_subplot(1, 1, 1)
