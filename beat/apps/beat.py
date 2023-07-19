255194fad7ce3473b2a8e8a57635ac04ad4c7162
hvasbath <Hannes.Bathke@gmx.net>
2023-07-19 13:48:44 +0200

    pyproject.toml: remove mpi4py dependeny - now optional

--- beat/apps/beat.py
+++ beat/apps/beat.py
@@ -2432,6 +2432,8 @@ def main():
             if acommand in subcommands:
                 globals()["command_" + acommand](["--help"])
 
+        if "mpi4py" in sys.modules:
+            print("MPI has been imported")
         sys.exit("Usage: %s" % usage)
 
     else:
