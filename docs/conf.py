c2c3b7f64b1107e0bdc8bbf433b733bd251b0247
hvasbath <Hannes.Bathke@gmx.net>
2025-01-05 22:24:02 +0100

    pyproject.toml: constrain pymc version, update references in webpage

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "beat"
-copyright = "2024, Hannes Vasyura-Bathke"
+copyright = "2025, Hannes Vasyura-Bathke"
 author = "Hannes Vasyura-Bathke"
 
 # The version info for the project you're documenting, acts as replacement for
